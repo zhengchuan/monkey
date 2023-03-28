b835342c25677387191afe9330c0d649da43883e
zhengchuan <1686640259@qq.com>
2023-03-28 08:48:17 +0800

    修复bug

--- 21tb_auto_learn.js
+++ 21tb_auto_learn.js
@@ -1,22 +1,18 @@
 // ==UserScript==
 // @name         时代光华课程自动学习
-// @namespace    http://tampermonkey.net/
-// @version      0.3
+// @namespace    https://github.com/zhengchuan
+// @version      0.1
 // @description  时代光华课程自动学习，每10分钟自动刷新页面（延迟30秒，预留网站计算时延），课程完成后自动点击下一步
 // @author       zhengchuan
 // @match        https://v4.21tb.com/els/html/courseStudyItem/courseStudyItem.learn.do*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=21tb.com
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @updateURL    https://github.com/zhengchuan/monkey/edit/main/21tb_auto_learn.js
-// @downloadURL  https://github.com/zhengchuan/monkey/edit/main/21tb_auto_learn.js
 // @grant        none
-// @license      MIT
+// @license MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
-    // 完成进度
-    let sr = document.getElementById("rms-studyRate").textContent;
     // 间隔次数计数
     let count = 0;
     // 定时任务间隔
@@ -28,6 +24,8 @@
 
     // 自动操作
     function auto(){
+        // 完成进度
+        let sr = document.getElementById("rms-studyRate").textContent;
         // 学习进度100%时自动点击下一步
         if(sr == '100'){
             $("#goNextStep").click();
@@ -35,6 +33,7 @@
         }
         // 630秒自动刷新页面
         count++;
+        console.log("当前自动计算第"+count+"次")
         if(count*offset/1000 > 630){
             location.reload();
         }
