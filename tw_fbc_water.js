edf9140469c00637ed38c1c32ee41e93b56c424b
zhengchuan <1686640259@qq.com>
2025-07-29 13:34:00 +0800

    增加金山文档

--- tw_fbc_water.js
+++ tw_fbc_water.js
@@ -1,21 +1,19 @@
 // ==UserScript==
 // @name         永祥去水印
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  去掉通威FBC、永祥ERP水印，阅读更丝滑
+// @version      1.3
+// @description  去掉通威、永祥内部系统去水印，阅读更丝滑，已实现FBC、ERP、金山
 // @author       ZhengChuan
 // @match        http://fbc.tongwei.com/*
 // @match        https://yxerp.tongwei.com/*
+// @match        https://tongwei.kdocs.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tongwei.com
 // @grant        none
 // @license      MIT
-// @downloadURL https://update.greasyfork.org/scripts/528730/%E9%80%9A%E5%A8%81FBC%E5%8E%BB%E6%B0%B4%E5%8D%B0.user.js
-// @updateURL https://update.greasyfork.org/scripts/528730/%E9%80%9A%E5%A8%81FBC%E5%8E%BB%E6%B0%B4%E5%8D%B0.meta.js
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
     let currentUrl = document.URL;
     if(currentUrl.startsWith("http://fbc.tongwei.com/")){
         setTimeout(function(){
@@ -32,6 +30,14 @@
                 waters[0].style.display='none';
             }
         },2500)
-
+    }
+    if(currentUrl.startsWith("https://tongwei.kdocs.cn/")){
+        setTimeout(function(){
+            let water = document.querySelector("#root > div.outline.pc.read-only.sidebar-visible > div.container-wrap > div.uikit-wo-watermark-plugin.uikit-watermark-container");
+            if(!!water){
+                alert("asdf");
+                water.remove();
+            }
+        },2500)
     }
 })();
