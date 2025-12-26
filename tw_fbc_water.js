6f7bc36d0e48cf25dffcaae04925bfb2f13e969c
zhengchuan <1686640259@qq.com>
2025-12-26 11:23:29 +0800

    增加fbcpub匹配，优化代码结构

--- tw_fbc_water.js
+++ tw_fbc_water.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         永祥去水印
+// @name         通威永祥去水印
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  去掉通威、永祥内部系统去水印，阅读更丝滑，已实现FBC、ERP、金山文档、知识中心
 // @author       ZhengChuan
 // @match        http://fbc.tongwei.com/*
@@ -9,6 +9,7 @@
 // @match        https://yxerp.tongwei.com/*
 // @match        https://tongwei.kdocs.cn/*
 // @match        https://zszx.tongwei.com/*
+// @match 		 https://fbcpub.tongwei.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tongwei.com
 // @grant        none
 // @license      MIT
@@ -19,43 +20,37 @@
 (function() {
     'use strict';
     let currentUrl = document.URL;
-    if(currentUrl.startsWith("http://fbc.tongwei.com/")){
-        setTimeout(function(){
-            let water = document.getElementById("1.23452384164.123412415");
-            if(!!water){
-                water.style.display='none';
-            }
-        },2500)
-    }else if(currentUrl.startsWith("https://fbc.tongwei.com")){
-        setTimeout(function(){
-            let water = document.getElementById("watermark");
-            if(!!water){
-                water.style.display='none';
-            }
-        },2500)
-    }else if(currentUrl.startsWith("https://zszx.tongwei.com/")){
-        setTimeout(function(){
-            let water = document.getElementById("Symbol\(watermark-dom\)");
-            if(!!water){
-                water.style.display='none';
-            }
-        },2500)
-    }
-    if(currentUrl.startsWith("https://yxerp.tongwei.com/")){
-         setTimeout(function(){
-            let waters = document.getElementsByClassName("T8JcTdAi");
-            if(waters.length>0){
-                waters[0].style.display='none';
-            }
-        },2500)
-    }
-    if(currentUrl.startsWith("https://tongwei.kdocs.cn/")){
-        setTimeout(function(){
-            let water = document.querySelector("#root > div.outline.pc.read-only.sidebar-visible > div.container-wrap > div.uikit-wo-watermark-plugin.uikit-watermark-container");
-            if(!!water){
-                alert("asdf");
-                water.remove();
-            }
-        },2500)
-    }
+	setTimeout(function(){
+		if(currentUrl.startsWith("http://fbc.tongwei.com/")){
+		let water = document.getElementById("1.23452384164.123412415");
+		if(!!water){
+			water.style.display='none';
+		}
+		}else if(currentUrl.startsWith("https://fbc.tongwei.com")){
+			let water = document.getElementById("watermark");
+			if(!!water){
+				water.style.display='none';
+			}
+		}else if(currentUrl.startsWith("https://zszx.tongwei.com/")){
+			let water = document.getElementById("Symbol\(watermark-dom\)");
+			if(!!water){
+				water.style.display='none';
+			}
+		}else if(currentUrl.startsWith("https://yxerp.tongwei.com/")){
+			let waters = document.getElementsByClassName("T8JcTdAi");
+			if(waters.length>0){
+				waters[0].style.display='none';
+			}
+		}else if(currentUrl.startsWith("https://tongwei.kdocs.cn/")){
+			let water = document.querySelector("#root > div.outline.pc.read-only.sidebar-visible > div.container-wrap > div.uikit-wo-watermark-plugin.uikit-watermark-container");
+			if(!!water){
+				water.remove();
+			}
+		}else if(currentUrl.startsWith("https://fbcpub.tongwei.com/")){
+			let water = document.querySelector("#notice1\\.23452384164\\.123412416");
+			if(!!water){
+				water.style.display='none';
+			}
+		}
+	},2500);
 })();
